2ec31b099170ca670abf20e78f5900c0482b217a
João Pedro <mendoncajoaopedro441@gmail.com>
2025-02-06 03:14:19 -0300

    create model

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    nickname = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=50, default='')
+    email = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return f'Nickname: {self.nickname} | Email: {self.email}'
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True)
+    title = models.CharField(max_length=255, default='')
+    description = models.CharField(max_length=255, default='')
